f2fd6a6d64a454a50a1f8c6ca3b3eedfa527412a
Hareesh kay <62635787+Hareeshkay@users.noreply.github.com>
2020-05-26 14:44:18 +0000

    Add files via upload

---
+++ Logstcbank.R
@@ -0,0 +1,29 @@
+library(readxl) #inorder to readxl file
+bank<-read_excel(file.choose()) #importing the data set
+View(bank)
+str(bank)
+attach(bank)
+#building model
+bank1<-glm(factor(y)~factor(job)+factor(marital)+factor(education)+factor(default)+balance+factor(housing)+factor(loan)+factor(contact)+day+factor(month)+duration+campaign+pdays+previous+factor(poutcome),family = "binomial",data = bank)
+summary(bank1)
+#finding probability value
+prob <- predict(bank1,type=c("response"),bank)
+prob
+#forming confusion matrix
+confusion<-table(prob>0.5,bank$y)
+probo <- prob>0.5
+table(probo)
+confusion
+
+# Model Accuracy 
+Accuracy<-sum(diag(confusion)/sum(confusion))
+Accuracy
+Error <- 1-Accuracy
+Error
+
+# ROC Curve 
+library(ROCR)
+rocrpred<-prediction(prob,bank$y)
+rocrperf<-performance(rocrpred,'tpr','fpr')
+plot(rocrperf,colorize=T,text.adj=c(-0.2,1.7))
+# More area under the ROC Curve better is the logistic regression model obtained
